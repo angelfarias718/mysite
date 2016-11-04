a0e06cea067f1cff51ffae8837511288e6e8924c
angelfarias718 <angelfarias718@gmail.com>
2016-11-03 20:47:37 -0400

    hwpt5wipcommit

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
 
-# Create your tests here.
+import datetime
+
+from django.utils import timezone
+from django.test import TestCase
+
+from .models import Question
+
+
+class QuestionMethodTests(TestCase):
+
+	def test_was_published_recently_with_future_question(self):
+		"""
+		was_published_recently() should return False for questions whose
+		pub_date is older than 1 day.
+		"""
+		time = timezone.now() + datetime.timedelta(days=30)
+		old_question = Question(pub_date=time)
+		self.assertIs(old_question.was_published_recently(), False)
+
+	def test_was_published_recently_with_future_question(self):
+		"""
+		was_published_recently() should return True for questions whose
+		pub_date is within the last day.
+		"""
+		time = timezone.now() - datetime.timedelta(hours=1)
+		recent.question = Question(pub_date=time)
+		self.assertIs(recent_question.was_published_recently(), True)
