2f7b1da3af77a939dd0586911238f6b843b71840
angelfarias718 <angelfarias718@gmail.com>
2016-11-10 15:14:17 -0500

    submissionhhw5

--- polls/views.py
+++ polls/views.py
@@ -10,6 +10,7 @@ from .models import Choice, Question
 
 from django.http import HttpResponse
 from django.template import loader
+from django.utils import timezone
 
 from .models import Question
 
@@ -18,12 +19,20 @@ class IndexView(generic.ListView):
 	context_object_name = 'latest_question_list'
 
 	def get_queryset(self):
-		"""Return the last five published questions."""
-		return Question.objects.order_by('-pub_date')[:5]
+		"""Return the last five published questions (mot including those set to be published in the future)."""
+		return Question.objects.filter(
+			pub_date__lte=timezone.now()
+		).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
 	model = Question
 	template_name = 'polls/detail.html'
+
+	def get_queryset(self):
+		"""
+		Excludes any questions that aren't published yet.
+		"""
+		return Question.objects.filter(pub_date__lte=timezone.now())
 
 class ResultsView(generic.DetailView):
 	model = Question
